6d9aaecb57c3a8ee716884b2d2d0ebdec1183b7a
Pranav-2608 <pranavchitlangia2608@gmail.com>
2024-04-19 20:03:58 +0530

    All setup configured

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-
+import express from "express"
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path:'./env'
 })
+
+const app = express();
 
 connectDB().then(()=>{
     app.listen(process.env.PORT || 8000,()=>{
